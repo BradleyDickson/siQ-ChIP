b40f448bfa709d26ba873e5f6a2643a4abcdd6f2
Bradley M Dickson <adynata@gmail.com>
2021-08-26 10:07:14 -0400

    allow different ct for IP and input
    ng/uL required for library quant

--- getalpha.f90
+++ getalpha.f90
@@ -31,21 +31,22 @@
       fil=par(11)*par(12)/par(9)/par(10)
       vr=par(13)/(par(14)-par(13))
       ct=par(16)
+      ctin=par(21)
       concin=par(9)
       concip=par(5)
       lengthin=par(24)
       lengthip=par(19)
       readin=1d0!unity
       readip=1d0
-      cexpectIN=1e-09*par(20)*2d0**ct &
-          /(dble(lengthin)*660d0)/(par(17)*1e-06)
-      cexpectIP=1e-09*par(15)*2d0**ct &
-          /(dble(lengthip)*660d0)/(par(17)*1e-06)
+!      cexpectIN=1e-09*par(20)*2d0**ct &
+!          /(dble(lengthin)*660d0)/(par(17)*1e-06)
+!      cexpectIP=1e-09*par(15)*2d0**ct &
+!          /(dble(lengthip)*660d0)/(par(17)*1e-06)
       cexpectIP= &
       par(15)*1e-09/(par(19)*660d0) *2d0**ct/(par(17)*1e-06)*1e+09
       actualIP=par(18)*1e-09*par(17)/(660d0*par(19))/(par(17)*1e-06)*1e+09
       cexpectIN= &
-      par(20)*1e-09/(par(24)*660d0) *2d0**ct/(par(22)*1e-06)*1e+09
+      par(20)*1e-09/(par(24)*660d0) *2d0**ctin/(par(22)*1e-06)*1e+09
       actualIN=par(23)*1e-09*par(22)/(660d0*par(24))/(par(22)*1e-06)*1e+09
 !      write(*,*) "expected and actual in nM"
 !      write(*,*) cexpectIP
@@ -53,7 +54,7 @@
 !      write(*,*) cexpectIN
 !      write(*,*) actualIN
 !      write(*,*) actualIN/cexpectIN
-      ampliin = 2d0**ct/((cexpectIN/actualIN)) !assumed in nM
+      ampliin = 2d0**ctin/((cexpectIN/actualIN)) !assumed in nM
       ampliip = 2d0**ct/((cexpectIP/actualIP)) !nM units!!!
       factr= readin*fi*fil*ampliin*vr/(fb*fbl*ampliip*readip)
       write(*,*) factr
